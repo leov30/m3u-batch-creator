ceac9c53b2efb13b8a1f70d2782eacca4411b6f8
leov30 <leo_v30@hotmail.com>
2022-09-10 21:30:47 -0700

    Add files via upload

--- m3u_creator.bat
+++ m3u_creator.bat
@@ -2,14 +2,19 @@
 if "%~1"=="" echo only drag and drop file or folder&pause&exit
 
 set "_game=%~n1"
-set "_game2="
+set "_match="
+set _flag=0
 
 if exist "%~1\" (
 	cd /d "%~1"&goto :next
 )
+::search for title and country code only if (Disc #) is at the end
+if "%_game%"=="%_game:(Disc 1).cue=%" (
+	for /f "tokens=1,2 delims=)(" %%g in ("%_game%") do set "_match=%%g(%%h)"
+)else (
+	set _flag=1
 
-::game name with only game name and country code
-for /f "tokens=1,2 delims=)(" %%g in ("%_game%") do set "_game2=%%g(%%h)"
+)
 
 :: folder name, m3u file name
 if not "%_game%"=="%_game:(Disc 1)=%" set "_game=%_game: (Disc 1)=%"&goto :next
@@ -19,12 +24,15 @@ if not "%_game%"=="%_game:(Disc 4)=%" set "_game=%_game: (Disc 4)=%"&goto :next
 if not "%_game%"=="%_game:(Disc 5)=%" set "_game=%_game: (Disc 5)=%"&goto :next
 if not "%_game%"=="%_game:(Disc 6)=%" set "_game=%_game: (Disc 6)=%"&goto :next
 :next
+echo No disc were found
+
+if %_flag% equ 1 set "_match=%_game%"
 
 type nul>"%temp%\temp.1"
-for %%g in ("%_game2%*.cue") do (
+for %%g in ("%_match%*.cue") do (
 	(echo %%g) >>"%temp%\temp.1"
 	
 )
-copy /y %temp%\temp.1 "%~dp0\%_game%.m3u"
+::copy to the game directory
+copy /y %temp%\temp.1 "%_game%.m3u"
 
-exit
