61003ca0a687827c0baf51e4f46d44bb176fb367
leov30 <leo_v30@hotmail.com>
2022-09-11 01:00:45 -0700

    Add files via upload

--- m3u_creator.bat
+++ m3u_creator.bat
@@ -1,15 +1,14 @@
 @echo off
-if "%~1"=="" echo only drag and drop file or folder&pause&exit
+::recursive mode
+if "%~1"=="" goto :recursive
+
+::folder mode
+if exist "%~1\" goto :folder
 
 set "_game=%~n1"
 set "_match="
-set "_folder="
 set _flag=0
 
-if exist "%~1\" (
-	set "_folder=%_game%\"
-	cd /d "%~1"&goto :next
-)
 ::search for title and country code only if (Disc #) is at the end
 if "%_game%"=="%_game:(Disc 1).cue=%" (
 	for /f "tokens=1,2 delims=)(" %%g in ("%_game%") do set "_match=%%g(%%h)"
@@ -17,7 +16,6 @@ if "%_game%"=="%_game:(Disc 1).cue=%" (
 	set _flag=1
 
 )
-
 :: folder name, m3u file name
 if not "%_game%"=="%_game:(Disc 1)=%" set "_game=%_game: (Disc 1)=%"&goto :next
 if not "%_game%"=="%_game:(Disc 2)=%" set "_game=%_game: (Disc 2)=%"&goto :next
@@ -26,18 +24,40 @@ if not "%_game%"=="%_game:(Disc 4)=%" set "_game=%_game: (Disc 4)=%"&goto :next
 if not "%_game%"=="%_game:(Disc 5)=%" set "_game=%_game: (Disc 5)=%"&goto :next
 if not "%_game%"=="%_game:(Disc 6)=%" set "_game=%_game: (Disc 6)=%"&goto :next
 :next
-echo No disc were found
+echo No disc file was found
 
 if %_flag% equ 1 set "_match=%_game%"
 
-type nul>"%temp%\temp.1"
+if exist "%_game%.m3u" del "%_game%.m3u"
 for %%g in ("%_match%*.cue") do (
-	(echo %_folder%%%g) >>"%temp%\temp.1"
+	(echo %%g) >>"%_game%.m3u"
 	
 )
+exit
+:: ---------------------- folder ----------------------------------------
+:folder
 
-if not "%_folder%"=="" cd..
+set "_folder=%~n1"
 
-::copy to the game directory
-copy /y %temp%\temp.1 "%_game%.m3u"
+if exist "%_folder%.m3u" del "%_folder%.m3u"
+for %%g in ("%_folder%\*.cue") do (
+	(echo %%g) >>"%_folder%.m3u"
+	
+)
+move /y "%_folder%.m3u" .
 
+exit
+:: ---------------------- recursive mode --------------------------------
+:recursive
+
+::recursive files mode
+if exist "*.cue" echo recursive mode
+
+for /d %%g in (*) do (
+	if exist "%%g.m3u" del "%%g.m3u"
+	
+	for %%h in ("%%g\*.cue") do (
+		(echo %%h) >>"%%g.m3u"
+		
+	)
+) 
