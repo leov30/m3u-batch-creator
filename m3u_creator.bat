898b30b3877c8775f193af666480c0bf6189a366
leov30 <leo_v30@hotmail.com>
2022-09-10 18:55:19 -0700

    Add files via upload

--- m3u_creator.bat
+++ m3u_creator.bat
@@ -1,12 +1,17 @@
 @echo off
-if "%~1"=="" echo only drag and drop file&pause&exit
+if "%~1"=="" echo only drag and drop file or folder&pause&exit
 
 set "_game=%~n1"
+set "_game2="
 
 if exist "%~1\" (
-	cd "%~1"&goto :next
+	cd /d "%~1"&goto :next
 )
 
+::game name with only game name and country code
+for /f "tokens=1,2 delims=)(" %%g in ("%_game%") do set "_game2=%%g(%%h)"
+
+:: folder name, m3u file name
 if not "%_game%"=="%_game:(Disc 1)=%" set "_game=%_game: (Disc 1)=%"&goto :next
 if not "%_game%"=="%_game:(Disc 2)=%" set "_game=%_game: (Disc 2)=%"&goto :next
 if not "%_game%"=="%_game:(Disc 3)=%" set "_game=%_game: (Disc 3)=%"&goto :next
@@ -16,8 +21,10 @@ if not "%_game%"=="%_game:(Disc 6)=%" set "_game=%_game: (Disc 6)=%"&goto :next
 :next
 
 type nul>"%temp%\temp.1"
-for %%g in ("%_game%*.cue") do (
+for %%g in ("%_game2%*.cue") do (
 	(echo %%g) >>"%temp%\temp.1"
-
+	
 )
 copy /y %temp%\temp.1 "%~dp0\%_game%.m3u"
+
+exit
